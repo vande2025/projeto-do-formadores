957eee64a4fa0655b6a127fe9a09fa4d987d4eb4
vande2025 <vanderlei.duarte@escola.pr.gov.br>
2025-06-23 19:19:01 -0300

    funcao

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ function inserirNome(){
 let nomeUsuario = prompt("Qual o seu nome?");
 let elemento = document.querySelector("#nome-usuario");
 elemento.textContent = nomeUsuario;
+
 insenrir nome();
+
 }
