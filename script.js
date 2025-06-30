29d13523fd6451fd29a7bf65a85f955c1dd1a51a
vande2025 <vanderlei.duarte@escola.pr.gov.br>
2025-06-30 19:19:43 -0300

    salvar

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ console.log(linguagens[2]);
 
 const item = document.querySelector("#lista");
 item.textContent = linguagens[0];
+
+let linguagens = ["Javascript", "Python", "C"];
+
+for (let i = 0; i < linguagens.length; i++) {
+    console.log(linguagens[i]);
+}
+let frutas = ["maça","banana","caju"];​
+console.log(frutas[1])banana
+
+console.log(frutas[0])maça
+
+console.log(frutas[2])caju
+
+console.log(frutas)['maça', 'banana', 'caju']
